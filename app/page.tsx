ddc90060c3bd4f19e1a18f22d659eefc4037642a
pqxglobal <l.r.slegt@gmail.com>
2023-06-08 22:36:08 +0200

    🚧 34:19

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-[2rem]">Hello World!</h1>
+
+      <hr />
+
+      <h2 className="text-2xl">I&apos;m Ready to Learn HTML</h2>
       <p>This is my first web page.</p>
+
+      <h3 className="text-[18.72px]">My Daily Schedule</h3>
+      <p>Let me tell you how I...</p>
+
+      <hr />
+
+      <h2 className="text-2xl">I&apos;m Ready to Learn HTML</h2>
+      <p>This is my first web page.</p>
+
+      <h3 className="text-[18.72px]">My Daily Schedule</h3>
+      <p>Let me tell you how I...</p>
     </>
   );
 }
